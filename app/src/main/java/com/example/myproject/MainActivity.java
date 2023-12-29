014a484c6cc0e86a63c1e4f88cf08dba467321a0
vuhieu <vutrunghieu24091999@gmail.com>
2023-12-29 17:10:52 +0700

    Hello

---
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.myproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.btnClick);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivityContent.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
